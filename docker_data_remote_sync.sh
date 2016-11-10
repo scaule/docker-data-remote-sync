53112feba1686e2af08432497110e9fa2222a69d
scaule <scaule@clever-age.com>
2016-11-10 12:12:01 +0100

    add script for remote sync

---
+++ docker_data_remote_sync.sh
@@ -0,0 +1,39 @@
+getLastDumpFileName() {
+	ssh -p$SERVER_PORT $SERVER_USER@$SERVER_NAME "ls -t $SERVER_DIRECTORY$1 | head -n1"
+}
+
+applyRestore() {
+	echo "use on restore $COMPOSE_PREFIX$1$COMPOSE_SUFFIX $2"
+	#./restore-docker.sh $COMPOSE_PREFIX$1$COMPOSE_SUFFIX $2
+}
+
+getDump() {
+	echo "downloading dump for $1"
+	scp -P$SERVER_PORT $SERVER_USER@$SERVER_NAME:$SERVER_DIRECTORY$1/$2 .
+	applyRestore $1 $2
+	mkdir -p "./data_current_file"
+	echo $2 > "./data_current_file/$service"
+}
+
+compareLastDumpFileWithCurrent() {
+	service=$1
+	remote_file_name=$(getLastDumpFileName $service)
+
+	if [ -f "./data_current_file/$service" ];
+	then
+		current_file_name=$(<"./data_current_file/$service")
+		if [ "$current_file_name" != "$remote_file_name" ];
+		then
+			getDump $service $remote_file_name
+		fi
+	else
+		getDump $service $remote_file_name
+	fi
+}
+
+. docker_data_remote_sync.cfg
+for i in "${CONTAINERS[@]}"
+do
+   :
+   compareLastDumpFileWithCurrent "$i"
+done
